59b88fe27262be8393c05cff390a56d5ce26e8b4
Shebil Antony <shebil.antony@stud.th-deg.de>
2026-03-30 18:15:12 +0200

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,24 @@
+# day2 - Variables & Data Types
+# robotics journey
+
+
+# string variables
+name = "Shebil"
+university = "Deggendorf Institute of Technology"
+location = "Deggendorf, Germany"
+
+# Number Variable
+age = 28
+study_hours = 3.5
+
+# Boolean Variables
+is_engineer = True
+loves_robotics = True
+
+print(f"My name is {name}")
+print(f"I study at {university}")
+print(f"I'm based in {location}")
+print(f"My age is {age}")
+print(f"I study {study_hours} hours daily")
+print(f"Am I an Engineer? {is_engineer}")
+print(f"Do I love Robotics? {loves_robotics}")
