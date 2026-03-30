1cf68f402c09bdeae41cd30421a32a62ec461009
Shebil Antony <shebil.antony@stud.th-deg.de>
2026-03-30 18:17:50 +0200

    Add files via upload

---
+++ prog1.py
@@ -0,0 +1,13 @@
+# Variables
+robot_name = "Atlas"
+robot_speed = 2.5
+battery_level = 85
+is_robot_moving = True
+is_robot_active = True
+current_location = "Deggendor Lab"
+
+print(f"Robot {robot_name} is at {current_location}")
+print(f"The Robot speed is {robot_speed}")
+print(f"Battery: {battery_level}")
+print(f"Is moving? {is_robot_moving}")
+print(f"Is active? {is_robot_active}")
